f86edc7c5c76ec803f5978ec9e29ded2df51485f
Willian Adonay Rosa Ramirez <willyrosaramirezc1@gmail.com>
2022-09-17 16:01:44 -0600

    preparando el entorno

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardProductComponent } from './card-product/card-product.component';
+
+
+
+@NgModule({
+  declarations: [
+    CardProductComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentsModule { }
